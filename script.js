e3f09dee6a7df02e18f2b28c0fbcbfc911d0e34f
Websites <nolant@icsz.ch>
2025-08-11 23:40:31 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const firebaseConfig = {
   apiKey: "AIzaSyAn6gP_ODz7Q02okhRAwD3gQbviLKI55ys",
-  authDomain: "https://tenron1844.github.io/Student-Lens/#",
+  authDomain: "https://tenron1844.github.io/Student-Lens/",
   projectId: "student-lens",
   storageBucket: "student-lens.appspot.com",
   messagingSenderId: "508910313071",
@@ -42,3 +42,4 @@ auth.onAuthStateChanged(user => {
 window.logout = function() {
   auth.signOut();
 };
+
